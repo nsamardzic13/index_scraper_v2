8caacaf1f3bea3552bdfe2b24bdaad91239d2064
Nikola Samardzic <nikola.samardzic1997@gmail.com>
2026-03-02 23:08:28 +0100

    Adding table cars

--- infrastructure/bigquery_cars.tf
+++ infrastructure/bigquery_cars.tf
@@ -1,21 +1,21 @@
-# resource "google_bigquery_table" "cars_parquet_external_table" {
-#   dataset_id          = google_bigquery_dataset.bigquery.dataset_id
-#   table_id            = "external_table_cars"
-#   deletion_protection = false
+resource "google_bigquery_table" "cars_parquet_external_table" {
+  dataset_id          = google_bigquery_dataset.bigquery.dataset_id
+  table_id            = "external_table_cars"
+  deletion_protection = false
 
-#   external_data_configuration {
-#     autodetect    = true
-#     source_format = "PARQUET"
+  external_data_configuration {
+    autodetect    = true
+    source_format = "PARQUET"
 
-#     # Use wildcards to match all parquet files in cars folder
-#     source_uris = [
-#       "gs://${google_storage_bucket.bucket.name}/data/cars/*.parquet"
-#     ]
+    # Use wildcards to match all parquet files in cars folder
+    source_uris = [
+      "gs://${google_storage_bucket.bucket.name}/data/cars/*.parquet"
+    ]
 
-#     # Skip header rows and ignore unknown values to handle empty buckets
-#     ignore_unknown_values = true
-#     max_bad_records       = 0
-#   }
+    # Skip header rows and ignore unknown values to handle empty buckets
+    ignore_unknown_values = true
+    max_bad_records       = 0
+  }
 
-#   depends_on = [google_storage_bucket.bucket]
-# }
+  depends_on = [google_storage_bucket.bucket]
+}
